d97d2a41426b0bcde948dcdd190c11de69190827
Whitneyakinyi2023 <whitneyologi@DESKTOP-53JJO2L>
2024-06-02 17:00:25 +0300

    task 1

--- web_dynamic/101-hbnb.py
+++ web_dynamic/101-hbnb.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 """ Starts a Flash Web Application """
+import uuid
+from flask import Flask, render_template
 from models import storage
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.place import Place
-from os import environ
-from flask import Flask, render_template
-import uuid
 app = Flask(__name__)
-
-# app.jinja_env.trim_blocks = True
-# app.jinja_env.lstrip_blocks = True
 
 
 @app.teardown_appcontext
@@ -20,7 +16,7 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/0-hbnb', strict_slashes=False)
+@app.route('/101-hbnb/', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -32,15 +28,15 @@ def hbnb():
 
     amenities = storage.all(Amenity).values()
     amenities = sorted(amenities, key=lambda k: k.name)
-
+    cache_id = str(uuid.uuid4())
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    return render_template('0-hbnb.html',
+    return render_template('101-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,
-                           cache_id=uuid.uuid4())
+                           cache_id=cache_id)
 
 
 if __name__ == "__main__":
--- web_dynamic/2-hbnb.py
+++ web_dynamic/2-hbnb.py
@@ -10,9 +10,6 @@ from flask import Flask, render_template
 import uuid
 app = Flask(__name__)
 
-# app.jinja_env.trim_blocks = True
-# app.jinja_env.lstrip_blocks = True
-
 
 @app.teardown_appcontext
 def close_db(error):
@@ -20,7 +17,7 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/0-hbnb', strict_slashes=False)
+@app.route('/2-hbnb', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -36,7 +33,7 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    return render_template('0-hbnb.html',
+    return render_template('2-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,
